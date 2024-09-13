2688b8dff48c1eb9cefc2f00bb153af3f7ef8ade
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-13 16:41:47 +0300

    Create test.py
    testing to see relationship between github and spyder compiler

---
+++ test.py
@@ -0,0 +1,7 @@
+"""
+This file has been created in the web version of github and 
+should be opened and edited on desktop version, 
+then run in the spyder application
+"""
+print("This is the first line to be printed.")
+print("This is the second line to be printed.")
