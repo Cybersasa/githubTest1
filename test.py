db8909b4e0ddc5f754241bd1d9180226a6d80fec
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-13 16:44:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ should be opened and edited on desktop version,
 then run in the spyder application
 """
 print("This is the first line to be printed.")
-print("This is the second line to be printed.")
+print("This is the second line to be printed.") 
